3104b96964bd63f78f11b86435a71d3709439012
Roman Simuta <simutaroman@gmail.com>
2022-07-06 20:52:47 +0300

    todos

--- src/dotnet/Blazor3D/Blazor3D/Core/BufferGeometry.cs
+++ src/dotnet/Blazor3D/Blazor3D/Core/BufferGeometry.cs
@@ -2,6 +2,9 @@
 
 namespace Blazor3D.Core
 {
+    /// <summary>
+    /// Buffer geometry abstract class
+    /// </summary>
     public abstract class BufferGeometry
     {
         protected BufferGeometry(string type)
@@ -9,8 +12,14 @@ namespace Blazor3D.Core
             Type = type;
         }
 
+        /// <summary>
+        /// Name des
+        /// </summary>
         public string Name { get; set; } = null!;
 
+        /// <summary>
+        /// Guid
+        /// </summary>
         public Guid Uuid { get; set; } = Guid.NewGuid();
 
         public string Type { get; } = "Geometry";
