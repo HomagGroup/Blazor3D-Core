0bdcfd0a47ba481c7e51be3ff3e558fc78281bb5
Roman Simuta <simutaroman@gmail.com>
2022-07-16 17:37:03 +0300

    capsule geometry added

---
+++ src/dotnet/Blazor3D/Blazor3D/Geometires/CapsuleGeometry.cs
@@ -0,0 +1,34 @@
+﻿using Blazor3D.Core;
+
+namespace Blazor3D.Geometires
+{
+    /// <summary>
+    /// <para>Class for a capsule with given radus and height. It is constructed using a lathe.</para>
+    /// </summary>
+    public class CapsuleGeometry : BufferGeometry
+    {
+        public CapsuleGeometry() : base("CapsuleGeometry")
+        {
+        }
+
+        /// <summary>
+        /// Radius of the capsule. Defaults to 1.
+        /// </summary>
+        public float Radius { get; set; } = 1;
+        /// <summary>
+        /// Length of the middle section. Defaults to 1.
+        /// </summary>
+        public float Length { get; set; } = 1;
+        /// <summary>
+        /// Number of curve segments used to build the caps. Defaults to 4.
+        /// </summary>
+        public int CapSegments { get; set; } = 4;
+        /// <summary>
+        /// Number of segmented faces around the circumference of the capsule. Defaults to 8.
+        /// </summary>
+        public int RadialSegments { get; set; } = 8;
+
+
+
+    }
+}
--- src/javascript/Builders/GeometryBuilder.js
+++ src/javascript/Builders/GeometryBuilder.js
@@ -1,31 +1,42 @@
 import * as THREE from "three";
 
-class GeometryBuilder{
-    static buildGeometry(options){
-        if(options.type == "BoxGeometry"){
-            const geometry = new THREE.BoxGeometry(
-                options.width,
-                options.height,
-                options.depth,
-                options.widthSegments,
-                options.heightSegments,
-                options.depthSegments
-              );
-              geometry.uuid = options.uuid;
-              return geometry;
-        }
-
-        if(options.type == "CircleGeometry"){
-            const geometry = new THREE.CircleGeometry(
-                options.radius,
-                options.segments,
-                options.thetaStart,
-                options.thetaLength,
-              );
-              geometry.uuid = options.uuid;
-              return geometry;
-        }
+class GeometryBuilder {
+  static buildGeometry(options) {
+    if (options.type == "BoxGeometry") {
+      const geometry = new THREE.BoxGeometry(
+        options.width,
+        options.height,
+        options.depth,
+        options.widthSegments,
+        options.heightSegments,
+        options.depthSegments
+      );
+      geometry.uuid = options.uuid;
+      return geometry;
     }
+
+    if (options.type == "CapsuleGeometry") {
+      const geometry = new THREE.CapsuleGeometry(
+        options.radius,
+        options.length,
+        options.capSubdivisions,
+        options.radialSegments
+      );
+      geometry.uuid = options.uuid;
+      return geometry;
+    }
+
+    if (options.type == "CircleGeometry") {
+      const geometry = new THREE.CircleGeometry(
+        options.radius,
+        options.segments,
+        options.thetaStart,
+        options.thetaLength
+      );
+      geometry.uuid = options.uuid;
+      return geometry;
+    }
+  }
 }
 
 export default GeometryBuilder;
