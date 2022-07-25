4ebbd1708f59d62c7e3bdc77bdb8679f1c596962
Roman Simuta <simutaroman@gmail.com>
2022-07-25 19:12:14 +0300

    Axes helper

---
+++ src/dotnet/Blazor3D/Blazor3D/Helpers/AxesHelper.cs
@@ -0,0 +1,32 @@
+﻿using Blazor3D.Core;
+
+namespace Blazor3D.Helpers
+{
+    /// <summary>
+    /// <para>An axis object to visualize the 3 axes in a simple way.</para>
+    /// <para>The X axis is red.The Y axis is green.The Z axis is blue.</para>
+    /// <para>This object inherits from <see cref="Object3D"/></para>
+    /// <para>Wrapper for three.js <a target="_blank" href="https://threejs.org/docs/index.html#api/en/helpers/AxesHelper">AxesHelper</a></para>
+    /// </summary>
+    public class AxesHelper : Object3D
+    {
+        public AxesHelper() : base("AxesHelper")
+        {
+        }
+
+        /// <summary>
+        /// Constructor with parameters
+        /// </summary>
+        /// <param name="size">Size of the lines representing the axes. Default is 1.</param>
+        public AxesHelper(double size = 1) : this()
+        {
+            Size = size;
+        }
+
+
+        /// <summary>
+        /// Size of the lines representing the axes. Default is 1.
+        /// </summary>
+        public double Size { get; set; } = 1;
+    }
+}
--- src/javascript/Builders/HelperBuilder.js
+++ src/javascript/Builders/HelperBuilder.js
@@ -4,14 +4,20 @@ class HelperBuilder{
 
     static BuildHelper(options){
         if (options.type == "ArrowHelper"){
-            console.log(options);
             const dir = new THREE.Vector3(options.dir.x, options.dir.y, options.dir.z);
             dir.normalize();
             const origin = new THREE.Vector3(options.origin.x, options.origin.y, options.origin.z);
             const arrow = new THREE.ArrowHelper(dir, origin, options.length, options.color, options.headLength, options.headWidth);
             arrow.uuid = options.uuid;
-            console.log(arrow);
             return arrow;
+        }
+
+        if (options.type == "AxesHelper"){
+            console.log(options);
+            const axes = new THREE.AxesHelper(options.size);
+            axes.uuid = options.uuid;
+            console.log(axes);
+            return axes;
         }
     }
 }
