8e0af1541fd301d78fbd9eeb8e0bd12c56f44718
Roman Simuta <simutaroman@gmail.com>
2022-07-31 22:02:49 +0300

    MeshStandardMaterial basic properties

--- src/dotnet/Blazor3D/Blazor3D/Materials/MeshStandardMaterial.cs
+++ src/dotnet/Blazor3D/Blazor3D/Materials/MeshStandardMaterial.cs
@@ -21,5 +21,31 @@ namespace Blazor3D.Materials
         /// Default value is "orange"</para>
         /// </summary>
         public string Color { get; set; } = "orange";
+
+        /// <summary>
+        /// <para>Define whether the material is rendered with flat shading. Default is false.</para>
+        /// </summary>
+        public bool FlatShading { get; set; } = false;
+
+        /// <summary>
+        /// <para>How much the material is like a metal. Non-metallic materials such as wood or stone use 0.0, metallic use 1.0, with nothing (usually) in between. Default is 0.0. A value between 0.0 and 1.0 could be used for a rusty metal look. If metalnessMap is also provided, both values are multiplied.</para>
+        /// </summary>
+        public float Metalness { get; set; } = 0;
+
+        /// <summary>
+        /// <para>How rough the material appears. 0.0 means a smooth mirror reflection, 1.0 means fully diffuse. Default is 1.0. If roughnessMap is also provided, both values are multiplied.</para>
+        /// </summary>
+        public float Roughness { get; set; } = 1;
+
+        /// <summary>
+        /// <para>Render geometry as wireframe. Default is false (i.e. render as flat polygons).</para>
+        /// </summary>
+        public bool Wireframe { get; set; } = false;
+
+        /// <summary>
+        /// <para>Controls wireframe thickness. Default is 1.</para>
+        /// <para>Due to limitations of the OpenGL Core Profile with the WebGL renderer on most platforms linewidth will always be 1 regardless of the set value.</para>
+        /// </summary>
+        public float WireframeLinecap { get; set; } = 1;
     }
 }
