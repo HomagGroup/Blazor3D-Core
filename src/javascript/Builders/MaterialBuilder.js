118c9ea43b59013f9d7bd90b6c12d58af6483f82
Roman Simuta <simutaroman@gmail.com>
2022-08-01 17:59:51 +0300

    MeshStandardMaterial basic properties (#21)

--- src/javascript/Builders/MaterialBuilder.js
+++ src/javascript/Builders/MaterialBuilder.js
@@ -5,6 +5,11 @@ class MaterialBuilder {
     if (options.type == "MeshStandardMaterial") {
       const material = new THREE.MeshStandardMaterial({
         color: options.color,
+        flatShading : options.flatShading,
+        metalness: options.metalness,
+        roughness: options.roughness,
+        wireframe: options.wireframe,
+        wireframeLinewidth: options.wireframeLinewidth
       });
       material.uuid = options.uuid;
       return material;
