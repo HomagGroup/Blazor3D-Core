894fade368f126b965697d23fb59d5a3d8271431
Roman Simuta <simutaroman@gmail.com>
2022-08-02 16:17:15 +0300

    Fix js module loading bug.

---
+++ src/dotnet/Blazor3D/Blazor3D/Events/LoadedModuleEventHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor3D.Events
+{
+    /// <summary>
+    /// <para>Delegate that handles <see cref="Viewers.Viewer"/> JsModuleLoaded event.</para>
+    /// </summary>
+    /// <returns>Task</returns>
+    public delegate Task LoadedModuleEventHandler();
+}
