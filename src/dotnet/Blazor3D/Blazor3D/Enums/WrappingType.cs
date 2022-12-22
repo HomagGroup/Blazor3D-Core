c616bf896e5e113ea5391901e167314514f4a417
Roman Simuta <simutaroman@gmail.com>
2022-12-22 12:44:01 +0200

    Basic texturing

---
+++ src/dotnet/Blazor3D/Blazor3D/Enums/WrappingType.cs
@@ -0,0 +1,21 @@
+﻿namespace Blazor3D.Enums
+{
+    /// <summary>
+    /// These define the texture's WrapS and WrapT properties, which define horizontal and vertical texture wrapping.
+    /// </summary>
+    public enum WrappingType
+    {
+        /// <summary>
+        /// The texture will simply repeat to infinity
+        /// </summary>
+        RepeatWrapping = 1000,
+        /// <summary>
+        /// The last pixel of the texture stretches to the edge of the mesh.
+        /// </summary>
+        ClampToEdgeWrapping = 1001,
+        /// <summary>
+        /// The texture will repeats to infinity, mirroring on each repeat.
+        /// </summary>
+        MirroredRepeatWrapping = 1002
+    }
+}
--- src/javascript/Builders/MaterialBuilder.js
+++ src/javascript/Builders/MaterialBuilder.js
@@ -1,14 +1,19 @@
 import * as THREE from "three";
+import TextureBuilder from "./TextureBuilder";
 
 class MaterialBuilder {
   static buildMaterial(options) {
     if (options.type == "MeshStandardMaterial") {
+
+      let map = TextureBuilder.buildTexture(options.map);
+      
       const material = new THREE.MeshStandardMaterial({
         color: options.color,
         flatShading : options.flatShading,
         metalness: options.metalness,
         roughness: options.roughness,
-        wireframe: options.wireframe
+        wireframe: options.wireframe,
+        map: map
       });
       material.uuid = options.uuid;
       return material;
