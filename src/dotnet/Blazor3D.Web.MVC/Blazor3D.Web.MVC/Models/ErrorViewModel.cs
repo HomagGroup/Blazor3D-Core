387104c69a391c000540cf7154f17d2bdd94e63d
Roman Simuta <simutaroman@gmail.com>
2022-12-23 14:23:59 +0200

    initial mvc app

---
+++ src/dotnet/Blazor3D.Web.MVC/Blazor3D.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blazor3D.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
