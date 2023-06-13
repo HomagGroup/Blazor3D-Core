1ad4cf9d83cd9facff2ba3eed5e4b15ebea258a1
Roman Simuta <simutaroman@hotmail.com>
2023-06-13 12:18:03 +0300

    basic shape and extrude geometries

---
+++ src/dotnet/Blazor3D/Blazor3D/Geometires/ShapeGeometry.cs
@@ -0,0 +1,29 @@
+﻿using Blazor3D.Core;
+using Blazor3D.Extras.Core;
+
+namespace Blazor3D.Geometires
+{
+    /// <summary>
+    /// Creates an one-sided polygonal geometry from one or more path shapes.
+    /// </summary>
+    public class ShapeGeometry : BufferGeometry
+    {
+        public ShapeGeometry() : base("ShapeGeometry")
+        {
+        }
+
+        /// <summary>
+        /// onstructor with parameters
+        /// </summary>
+        /// <param name="shape"><see cref="Shape"/> polygonal shape.</param>
+        public ShapeGeometry(Shape shape):this()
+        {
+            Shape = shape;
+        }
+
+        /// <summary>
+        /// <see cref="Shape"/> polygonal shape.
+        /// </summary>
+        public Shape Shape { get; set; } = new Shape();
+    }
+}
