f2522254fb968a2c8e3ef99b3dfcc9bbadd9d05f
Roman Simuta <simutaroman@gmail.com>
2023-09-27 10:12:40 +0300

    Revert "Added transparent background support (#48)"
    This reverts commit 5f3b808f5bc769025d0259ce3d84ed5bc9b34190.

--- src/dotnet/Blazor3D/Blazor3D/Settings/WebGLRendererSettings.cs
+++ src/dotnet/Blazor3D/Blazor3D/Settings/WebGLRendererSettings.cs
@@ -9,15 +9,5 @@
         /// Whether to perform antialiasing. Default is true.
         /// </summary>
         public bool Antialias { get; set; } = true;
-
-        /// <summary>
-        /// controls the default clear alpha value. When set to true, the value is 0. Otherwise it's 1. Default is false.
-        /// </summary>
-        public bool Alpha { get; set; } = false;
-        
-        /// <summary>
-        /// whether the renderer will assume that colors have premultiplied alpha. Default is true.
-        /// </summary>
-        public bool PremultipliedAlpha { get; set; } = true;
     }
 }
